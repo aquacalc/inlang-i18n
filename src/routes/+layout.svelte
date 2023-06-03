4fbaa26b1f079ab6f7853fb667dde7fd586db092
aquacalc <aquacalc@yahoo.com>
2023-06-03 18:12:04 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,152 @@
+<script>
+	// import { onMount } from 'svelte';
+
+	// // see: https://vercel.com/docs/concepts/analytics/audiences/quickstart
+	// import { dev } from '$app/environment';
+	// import { inject } from '@vercel/analytics';
+
+	import { fade } from 'svelte/transition';
+	// import Moon from '$lib/icons/moon.svelte';
+	// import Sun from '$lib/icons/sun.svelte';
+
+	// export let data;
+
+	// inject({ mode: dev ? 'development' : 'production' });
+
+	// let currentTheme = '';
+
+	// onMount(() => {
+	// 	// currentTheme = document.documentElement.dataset.theme;
+	// 	const userPrefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+	// 	const hasUserSetDarkModeManually = document.documentElement.dataset.theme == 'dark';
+	// 	if (!hasUserSetDarkModeManually) {
+	// 		setTheme(userPrefersDarkMode ? 'dark' : 'light');
+	// 	}
+	// });
+
+	// // see: https://www.youtube.com/watch?v=mt56gKUeppk&list=PLm_Qt4aKpfKjf77S8UD79Ockhwp_699Ms&index=20
+	// const setTheme = (theme) => {
+	// 	// document.cookie = `siteTheme=${theme};Max-Age=31536000;path="/"`;
+
+	// 	// Access HTML tag
+	// 	document.documentElement.dataset.theme = theme;
+	// 	// Set cookie for SSR persistence
+	// 	document.cookie = `siteTheme=${theme}; max-age=31536000; path="/"`;
+	// 	// document.cookie = `siteTheme=${theme}; Max-Age=1704085200; path="/"`;
+	// 	currentTheme = theme;
+	// };
+
+	// see: https://www.youtube.com/watch?v=Y6IbPfMU1xM
+	import { i, languages, language, switchLanguage } from '@inlang/sdk-js';
+
+	// console.log(`languages: `, languages);
+
+	// --------------- //
+
+	// Make this assignment to show correct select selection on refresh
+	let selectedLang = language;
+
+	const handleLanguageSelection = async (e) => {
+		// console.log(`1. language = ${language} (vs. ${e.target.value})`)
+		await switchLanguage(e.target.value);
+	};
+</script>
+
+<nav>
+	<ul id="nav-ul" style="background-color: #555; height: 60px;">
+		<li>
+			<a href="/" target="_blank" rel="noopener noreferrer"
+				>WQ
+				<!-- ><img id="recycle-drop-menu" src="/recycle-drop.png" alt="WQ | FAQ icon" /> WQ -->
+				<span style="color: orange;">|</span> FAQ Blog</a
+			>
+		</li>
+		<li>
+			<div style="text-align: right; margin-right: 20px;">
+				<select
+					name="langs"
+					id="langs"
+					on:change={handleLanguageSelection}
+					bind:value={selectedLang}
+				>
+					<option value="en">English</option>
+					<option value="de">Deutsch</option>
+					<option value="ar">اللغة العربية</option>
+					<option value="fr">Français</option>
+					<option value="es">Español</option>
+					<option value="il">עִברִית</option>
+					<option value="gr">Ελληνικά</option>
+					<option value="br">Brasileiro</option>
+				</select>
+			</div>
+		</li>
+		<!-- <li class="relative">
+			{#if currentTheme == 'light'}
+				<a class="moon" href={'#'} on:click={() => setTheme('dark')}>
+					<Moon />
+				</a>
+			{:else}
+				<a class="sun" href={'#'} on:click={() => setTheme('light')}>
+					<Sun />
+				</a>
+			{/if}
+		</li> -->
+	</ul>
+</nav>
+
+<!-- {#key data.currentRoute} -->
+<main in:fade={{ duration: 350 }}>
+	<slot />
+</main>
+
+<!-- {/key} -->
+
+<style>
+	ul {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		padding: 0 30px;
+		gap: 4rem;
+		list-style-type: none;
+	}
+
+	a {
+		color: #ddd;
+		text-decoration: none;
+		font-size: 1.2rem;
+		opacity: 0.9;
+	}
+
+	a:hover {
+		color: #fff;
+		opacity: 1;
+	}
+
+	select {
+		font-size: 1.35rem;
+		border-radius: 8px;
+		padding: 2px;
+    background: transparent;
+    color: #ccc;
+    cursor: pointer;
+	}
+
+  select:hover {
+    color: #eee;
+  }
+
+	select:focus {
+		outline: none;
+	}
+
+	/* #recycle-drop-menu {
+		width: 16px;
+		padding-right: 2px;
+		opacity: 0.9;
+	} */
+
+	/* a #recycle-drop-menu:hover {
+		opacity: 1;
+	} */
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+	// see: https://www.youtube.com/watch?v=Y6IbPfMU1xM
+	import { i, languages, language, switchLanguage } from '@inlang/sdk-js';
+
+	// console.log(`languages: `, languages);
+</script>
+
+<!-- 
+  "flag": "🇨🇦", 
+  "flag": "🇩🇪",
+  "flag": "🇪🇸",
+  "flag": "🇫🇷",
+  "flag": "🇸🇦",
+ -->
+
+<div class="hero" style="margin-bottom: 1.25rem;">
+	<h1 style="text-align: center;">{i('hero')}</h1>
+</div>
+
+<p>
+	{@html i('welcome')} 
+</p>
+
+<div
+	style="font-size: 1.35rem; width: 90%; margin: 0 auto; text-align: {language === 'ar' ||
+	language === 'il'
+		? 'right'
+		: 'left'};"
+>
+	<section style="font-size: 1.5rem; margin: 0rem 6.15rem; line-height: 2.25rem;">
+		<p class="viz-para" style="margin-top: 0;">{i('p1')}</p>
+		<p class="viz-para" style="margin-top: 0;">{i('p2')}</p>
+		<p class="viz-para" style="margin-top: 0;">{i('p3')}</p>
+		<blockquote>
+			<p class="viz-para" style="margin-bottom: 0; font-size: 1.35rem; line-height: 3rem;">
+				{i('block')}
+			</p>
+		</blockquote>
+	</section>
+</div>
+
+<style>
+	.hero {
+		height: 20vh;
+		display: flex;
+		place-items: center;
+		flex-direction: column;
+		justify-content: center;
+		text-align: center;
+		/* background-color: var(--bkg-hero); */
+		background: linear-gradient(to right, #385c9e, #486bad);
+		color: #eee;
+		font-family: sans-serif;
+		margin-bottom: 1rem;
+	}
+
+	.hero h1 {
+		/* margin-top: 0; */
+		font-weight: 300;
+		font-size: 3rem;
+		color: #fff;
+	}
+
+	/* select {
+		font-size: 1.5rem;
+		border-radius: 8px;
+		padding: 2px;
+	} */
+
+	blockquote {
+		border-left: 4px solid rebeccapurple;
+		border-right: 4px solid rebeccapurple;
+		/* border-left: 4px solid var(--about-text-highlight);
+		border-right: 4px solid var(--about-text-highlight); */
+		border-radius: 8px;
+		font-family: sans-serif;
+		margin: 2rem 0;
+		padding: 0.35rem;
+		padding-left: 0.5rem;
+		padding-right: 0.5rem;
+		background-color: #ebf2fe;
+		/* background-color: var(--bkg-blockquote); */
+	}
+
+	blockquote .viz-para {
+		/* color: var(--about-text-highlight) */
+		color: #555;
+		/* color: var(--text-blockquote); */
+		padding-bottom: 0.35rem;
+	}
+</style>
