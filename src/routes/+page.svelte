b3cbe067bcd8ca2d106d0a214f235591daac52fd
aquacalc <aquacalc@yahoo.com>
2023-06-04 08:35:08 -0400

    add <section>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,6 +43,55 @@
 	</section>
 </div>
 
+<section
+	id="top-section"
+	style="text-align: left; font-size: 1.5rem; margin: 2rem 10rem 0 10rem; line-height: 2.5rem;"
+>
+	<hr style="width: 60%; margin-bottom: 35px; opacity: 1;" />
+
+	<p class="viz-para">
+		{@html i('end-p1-a')}
+		<svg
+			version="1.1"
+			class="fa-icon svelte-1dof0an"
+			width="18.285714285714285"
+			height="32"
+			role="presentation"
+			viewBox="0 0 1024 1792"
+			style="fill: green; opacity: 1; cursor: none;font-size: 2em"
+			><path
+				d="M768 640q0-106-75-181t-181-75-181 75-75 181 75 181 181 75 181-75 75-181zM1024 640q0 109-33 179l-364 774q-16 33-47.5 52t-67.5 19-67.5-19-46.5-52l-365-774q-33-70-33-179 0-212 150-362t362-150 362 150 150 362z"
+			/>
+		</svg>
+		{@html i('end-p1-b')}
+	</p>
+
+	<div style="text-align: center;">
+		<img style="width: 80%;" src="/wq-map-quantitative.png" alt="The Water Quality Map - example" />
+	</div>
+
+	<p class="viz-para">
+		{@html i('end-p2')}
+	</p>
+
+	<hr style="width: 60%; margin-bottom: 35px; opacity: 1;" />
+
+	<p class="viz-para">{@html i('end-p3')}</p>
+
+	<p class="viz-para">
+		{@html i('end-p4')}
+	</p>
+
+	<p class="viz-para">
+		{@html i('end-p5-a')}<a
+			style="font-family: sans-serif; font-size: 1.35rem;"
+			href="https://www.wq-faq.com"
+			target="_blank"
+			rel="noopener noreferrer">{@html i('end-p5-b')}</a
+		>{i('end-p5-c')}
+	</p>
+</section>
+
 <footer>
 	<p>
 		&copy; {new Date().getFullYear()}
